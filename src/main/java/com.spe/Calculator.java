e8829a55cc4ac314bdd3acdc14247d5d9e9770e9
nidhibudhraja <nidhi.budhraja96@gmail.com>
2020-05-10 17:45:25 +0530

    added code and test for other arithmetic operation to check if the pipeline triggers

--- src/main/java/com.spe/Calculator.java
+++ src/main/java/com.spe/Calculator.java
@@ -3,7 +3,28 @@ package com.spe;
 public class Calculator {
     public static void main(String[] args) {
         System.out.println("add 6+4="+add(6,4));
+        System.out.println("subtraction 6-4="+subtract(6,4));
+        System.out.println("multiplication 6*4="+multiply(6,4));
+        System.out.println("division 16/4="+divide(16,4));
     }
+
+    public static int divide(int i, int i1) {
+        int q;
+        if(i1!=0)
+            q=i/i1;
+        else
+            q=0;
+        return q;
+    }
+
+    public static int multiply(int i, int i1) {
+        return i*i1;
+    }
+
+    public static int subtract(int i, int i1) {
+        return i-i1;
+    }
+
     public static int add(int a,int b){
         return a+b;
     }
--- src/test/java/com.spe/CalculatorTests.java
+++ src/test/java/com.spe/CalculatorTests.java
@@ -18,4 +18,34 @@ public class CalculatorTests {
         long obtained_sum=calc.add(a,b);
         Assert.assertEquals(expected_sum,obtained_sum);
     }
+    @Test
+    public void subtractTest()throws IOException{
+        int a= 7;
+        int b=9;
+        int expected_diff=-2;
+        long obtained_diff=calc.subtract(a,b);
+        Assert.assertEquals(expected_diff,obtained_diff);
+    }
+    @Test
+    public void multiplyTest()throws IOException{
+        int a= 7;
+        int b=9;
+        int expected_product=63;
+        long obtained_product=calc.multiply(a,b);
+        Assert.assertEquals(expected_product,obtained_product);
+    }
+    @Test
+    public void divideTest()throws IOException{
+        int a= 27;
+        int b=9;
+        int expected_q=3;
+        long obtained_q=calc.divide(a,b);
+        Assert.assertEquals(expected_q,obtained_q);
+    }
+    @Test
+    public void nonZeroDivisorTest() throws IOException{
+        int a=4;
+        int b=0;
+        Assert.assertEquals("Divisor should be a non zero number",0,calc.divide(a,b));
+    }
 }
